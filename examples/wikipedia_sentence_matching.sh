00f9e7703369b84546d107b501b15a0d7e15f111
Jason Weston <jase@fb.com>
2017-09-15 23:46:27 -0400

    eval

---
+++ examples/wikipedia_sentence_matching.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
+DATASET=(
+  wikipedia
+)
+
+MODELDIR=/tmp/starspace/models
+DATADIR=/tmp/starspace/data
+
+mkdir -p "${MODELDIR}"
+mkdir -p "${DATADIR}"
+
+echo "Downloading dataset ag_news"
+if [ ! -f "${DATADIR}/${DATASET[i]}_shuf_train250k.txt" ]
+then
+    wget -c "https://s3.amazonaws.com/fair-data/starspace/wikipedia_trn250k.tgz" -O "${DATADIR}/${DATASET[0]}_train.tar.gz"
+    tar -xzvf "${DATADIR}/${DATASET[0]}_train.tar.gz" -C "${DATADIR}"
+    wget -c "https://s3.amazonaws.com/fair-data/starspace/wikipedia_devtst.tgz" -O "${DATADIR}/${DATASET[0]}_test.tar.gz"
+    tar -xzvf "${DATADIR}/${DATASET[0]}_test.tar.gz" -C "${DATADIR}"
+  fi
+
+echo "Compiling StarSpace"
+
+make
+
+echo "Start to train on wikipedia data (small training set example version, not the same as the paper which takes longer to run on a bigger training set):"
+
+./starspace train \
+  -trainFile "${DATADIR}"/wikipedia_shuf_train250k.txt \
+  -model "${MODELDIR}"/wikipedia_sentence_match \
+  -trainMode 3 \
+  -initRandSd 0.01 \
+  -adagrad true \
+  -ngrams 1 \
+  -lr 0.05 \
+  -epoch 5 \
+  -thread 20 \
+  -dim 50 \
+  -negSearchLimit 10 \
+  -fileFormat labelDoc \
+  -similarity "cosine" \
+  -minCount 5 \
+  -verbose true
+
+echo "Start to evaluate trained model:"
+
+./starspace test \
+  -testFile "${DATADIR}"/wikipedia_test10k.txt \
+  -basedoc "${DATADIR}"/wikipedia_test_basedocs.txt \
+  -model "${MODELDIR}"/wikipedia_sentence_match \
+  -thread 20 \
+  -trainMode 3 \
+  -verbose true
